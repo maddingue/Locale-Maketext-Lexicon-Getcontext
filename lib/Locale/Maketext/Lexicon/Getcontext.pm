b8eb7ab0d8e28a2511d7c7784e56405e25c42807
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2017-01-12 21:52:03 +0100

    set release date & version

--- lib/Locale/Maketext/Lexicon/Getcontext.pm
+++ lib/Locale/Maketext/Lexicon/Getcontext.pm
@@ -4,7 +4,7 @@ use strict;
 
 use Locale::Maketext::Lexicon::Gettext;
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 my ( $InputEncoding, $OutputEncoding, $DoEncoding );
 
 sub input_encoding  {$InputEncoding}
